5e76eb21aea6ca208e3bb28dacfee732e8b93f87
Giancky79 <46261713+Giancky79@users.noreply.github.com>
2019-09-24 11:34:44 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#è in completamento, se usato non andrà
+
+
+TIME=$(date +"%d-%m-%y")
+DAYS=(Mon Tue Wed Thu Fri Sat Sun)
+FILENAME=backup-$TIME.tar
+SRCDIR=/root/docker/hass
+DESDIR=/root/backup
+tar -czvf $DESDIR/$FILENAME --exclude={deps,*.db,*.log} $SRCDIR
