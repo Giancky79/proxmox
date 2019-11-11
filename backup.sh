ecfdc0e53a00f5e9c61f84452baba012e21fbea8
Giancky79 <46261713+Giancky79@users.noreply.github.com>
2019-11-11 09:38:06 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 
 TIME=$(date +"%d-%m-%y")
 DAYS=(Mon Tue Wed Thu Fri Sat Sun)
-FILENAME=backup-$TIME.tar
+FILENAME=backup-$TIME-$DAYS.tar
 SRCDIR=/root/docker/hass
 DESDIR=/root/backup
 tar -czvf $DESDIR/$FILENAME --exclude={deps,*.db,*.log} $SRCDIR
